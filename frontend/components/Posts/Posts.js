d7862350be0dd537973d0ce1ba93550e92513167
Matt Glenn <highopenended@gmail.com>
2024-06-17 11:47:59 -0400

    Got the posts to show up

--- frontend/components/Posts/Posts.js
+++ frontend/components/Posts/Posts.js
@@ -3,10 +3,15 @@ import Post from './Post';
 
 const Posts = (props) => {
   // 🔥 Make sure the parent of Posts is passing the right props!
-  const { likePost, posts } = props;
+  const { likePost, posts } = props
 
   return (
     <div className='posts-container-wrapper'>
+      {posts.map(ps=>{
+        console.log(ps)
+        return <Post key={ps.id} post={ps} likePost={likePost}/>
+      })}
+
       {/* Map through the posts array returning a Post component at each iteration */}
       {/* Check the implementation of Post to see what props it requires! */}
     </div>
